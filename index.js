62c467cd4a1a6a838d816ae7e8d10eebb80658a2
Chayan2002 <93382253+chayansurana3@users.noreply.github.com>
2023-01-01 10:54:45 +0530

    Updating index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor(6 * Math.random()) + 1;
 var randomNumber2 = Math.floor(6 * Math.random()) + 1;
-document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.getElementsByClassName("img1")[0].setAttribute("src", "./dice" + randomNumber1 + ".png");
+document.getElementsByClassName("img2")[0].setAttribute("src", "./dice" + randomNumber2 + ".png");
 if (randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 Wins";
 }else if (randomNumber2 > randomNumber1){

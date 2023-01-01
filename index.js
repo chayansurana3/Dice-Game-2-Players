6ac1c454bfab1f535d1a9492cfa7c282d88d0f58
Chayan2002 <93382253+chayansurana3@users.noreply.github.com>
2023-01-01 10:53:35 +0530

    Uploading Game Files

---
+++ index.js
@@ -0,0 +1,11 @@
+var randomNumber1 = Math.floor(6 * Math.random()) + 1;
+var randomNumber2 = Math.floor(6 * Math.random()) + 1;
+document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}else {
+    document.querySelector("h1").innerHTML = "Match DRAW";
+}
